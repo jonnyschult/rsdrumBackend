3812a9d3d839e06bd2edf802d51168713a76076a
Jonny <jonathon.schult@gmail.com>
2021-07-08 17:23:03 -0500

    removed dist file. Added console log to db for trouble shooting

--- dist/middleware/headers.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const headers = (req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    return next();
-};
-exports.default = headers;
--- src/db.ts
+++ src/db.ts
@@ -2,6 +2,8 @@ import { DynamoDB } from "aws-sdk";
 import * as dotenv from "dotenv";
 dotenv.config();
 
+console.log("In DB", process.env.DB_ENDPOINT);
+
 const documentClient = new DynamoDB.DocumentClient({
   region: "us-east-2",
   endpoint: process.env.DB_ENDPOINT,
